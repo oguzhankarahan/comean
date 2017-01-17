970e975a8a1cb6e30afe99b7c049d8a6dfbb3235
oguzhancolak <oguzhan.colak@in2project.com>
2017-01-17 17:31:50 +0300

    Project updated

--- server/config/local.env.js
+++ server/config/local.env.js
@@ -6,9 +6,8 @@
 // You will need to set these on the server you deploy to.
 
 module.exports = {
-  DOMAIN:           'http://localhost:9010',
-  SESSION_SECRET:   'commonity-secret',
-
+  DOMAIN: 'http://localhost:9010',
+  SESSION_SECRET: "commonity-secret",
   // Control debug level for modules using visionmedia/debug
   DEBUG: ''
 };
